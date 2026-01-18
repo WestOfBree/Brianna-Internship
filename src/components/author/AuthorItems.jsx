5d6366526fdf383a8634598cd0971d87d0346c25
WestOfBree <b.thomas1996@yahoo.com>
2026-01-18 18:51:05 -0500

    Author page displaying static API data, stuck on making it dynamic.

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -2,18 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import Skeleton from "../UI/Skeleton";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const AuthorItems = () => {
+const AuthorItems = ({ authorData }) => {
+ const items= authorData.nftCollection;
+
   return (
     <div className="de_tab_content">
       <div className="tab-1">
         <div className="row">
-          {new Array(8).fill(0).map((_, index) => (
+          {items.map((item, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft__item">
                 <div className="author_list_pp">
                   <Link to="">
-                    <img className="lazy" src={AuthorImage} alt="" />
+                    <img className="lazy" src={authorData.authorImage} alt="" />
                     <i className="fa fa-check"></i>
                   </Link>
                 </div>
@@ -37,7 +42,7 @@ const AuthorItems = () => {
                   </div>
                   <Link to="/item-details">
                     <img
-                      src={nftImage}
+                      src={item.nftImage}
                       className="lazy nft__item_preview"
                       alt=""
                     />
@@ -45,12 +50,12 @@ const AuthorItems = () => {
                 </div>
                 <div className="nft__item_info">
                   <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{item.title}</h4>
                   </Link>
-                  <div className="nft__item_price">2.52 ETH</div>
+                  <div className="nft__item_price">{item.price} ETH</div>
                   <div className="nft__item_like">
                     <i className="fa fa-heart"></i>
-                    <span>97</span>
+                    <span>{item.likes}</span>
                   </div>
                 </div>
               </div>
--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -3,8 +3,87 @@ import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
 import { Link } from "react-router-dom";
 import AuthorImage from "../images/author_thumbnail.jpg";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Skeleton from "../components/UI/Skeleton";
 
 const Author = () => {
+  const [authorData, setAuthorData] = useState(null);
+  const [loading , setLoading] = useState(true);
+  useEffect(() => {
+    axios
+    .get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${authorId}`)
+      .then((response) => {
+        setAuthorData(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching author data:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <div id="wrapper">
+        <div className="no-bottom no-top" id="content">
+          <div id="top"></div>  
+          <section>
+            <Skeleton width={"100%"} height={"400px"} borderRadius={"0px"} />
+          </section>
+
+          <section aria-label="section">    
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="d_profile de-flex">
+                    <div className="de-flex-col">
+                      <div className="profile_avatar">  
+                        <Skeleton width={"150px"} height={"150px"} borderRadius={"50%"} />                     
+                        <div className="profile_name">
+                          <h4>  
+                            <Skeleton width={"200px"} height={"20px"} borderRadius={"5px"} />
+                            <span className="profile_username">
+                              <Skeleton width={"150px"} height={"15px"} borderRadius={"5px"} />
+                            </span>
+                            <span id="wallet" className="profile_wallet">
+                              <Skeleton width={"250px"} height={"15px"} borderRadius={"5px"} /> 
+                            </span>
+                            <button id="btn_copy" title="Copy Text">
+                              <Skeleton width={"50px"} height={"20px"} borderRadius={"5px"} />
+                            </button>
+                          </h4>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="profile_follow de-flex">
+                      <div className="de-flex-col">
+                        <div className="profile_follower">
+                          <Skeleton width={"100px"} height={"15px"} borderRadius={"5px"} />
+                        </div>
+                        <Link to="#" className="btn-main">
+                          <Skeleton width={"80px"} height={"30px"} borderRadius={"5px"} />
+
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-12">
+                  <div className="de_tab tab_simple">
+                    <Skeleton width={"100%"} height={"400px"} borderRadius={"10px"} />  
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    );
+  }
+
+    else{
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
@@ -25,15 +104,15 @@ const Author = () => {
                 <div className="d_profile de-flex">
                   <div className="de-flex-col">
                     <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
+                      <img src={authorData.authorImage} alt="" />
 
                       <i className="fa fa-check"></i>
                       <div className="profile_name">
                         <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
+                          {authorData.authorName}
+                          <span className="profile_username">@{authorData.tag}</span>
                           <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
+                            {authorData.address}
                           </span>
                           <button id="btn_copy" title="Copy Text">
                             Copy
@@ -44,7 +123,7 @@ const Author = () => {
                   </div>
                   <div className="profile_follow de-flex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
+                      <div className="profile_follower">{authorData.followers} followers</div>
                       <Link to="#" className="btn-main">
                         Follow
                       </Link>
@@ -55,7 +134,7 @@ const Author = () => {
 
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
-                  <AuthorItems />
+                  <AuthorItems authorData={authorData} loading={loading}/>
                 </div>
               </div>
             </div>
@@ -64,6 +143,7 @@ const Author = () => {
       </div>
     </div>
   );
+  }
 };
 
 export default Author;
